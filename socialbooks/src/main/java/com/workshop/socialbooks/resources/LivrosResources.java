4dc53b976e63ecf68a51a41d09e007a9d8cf42d5
Raphael Barreto <tnt.raphael@gmail.com>
2016-04-04 16:11:57 -0300

    Salvando o recurso Livro a partir de um POST

--- socialbooks/src/main/java/com/workshop/socialbooks/resources/LivrosResources.java
+++ socialbooks/src/main/java/com/workshop/socialbooks/resources/LivrosResources.java
@@ -3,6 +3,7 @@ package com.workshop.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,19 @@ import com.workshop.socialbooks.domain.Livro;
 import com.workshop.socialbooks.repository.LivrosRepository;
 
 @RestController
+@RequestMapping("/livros")
 public class LivrosResources {
 
 	@Autowired
 	private LivrosRepository livrosRepository;
 
-	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro> listar() {
-
 		return livrosRepository.findAll();
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) {
+		livrosRepository.saveAndFlush(livro);
 	}
 }
