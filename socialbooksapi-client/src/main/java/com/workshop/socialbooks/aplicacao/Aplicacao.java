b46a139adcd44acc7756678376bc0c1940bc8b21
Raphael Barreto <tnt.raphael@gmail.com>
2016-04-08 08:57:04 -0300

    Criação do client para consumo da API

---
+++ socialbooksapi-client/src/main/java/com/workshop/socialbooks/aplicacao/Aplicacao.java
@@ -0,0 +1,24 @@
+package com.workshop.socialbooks.aplicacao;
+
+import java.net.URI;
+
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+
+		RestTemplate restTemplate = new RestTemplate();
+
+		RequestEntity<Void> request = RequestEntity.get(URI.create("http://localhost:8080/livros"))
+				.header("Authorization", "Basic cmFwaGFlbDpzM25oNA==").build();
+
+		ResponseEntity<Livro[]> response = restTemplate.exchange(request, Livro[].class);
+
+		for (Livro livro : response.getBody()) {
+			System.out.println("Livro:" + livro.getNome());
+		}
+	}
+}
